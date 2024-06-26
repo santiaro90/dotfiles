8749685479723d7b8492344ebf984be51e6a7028
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2024-06-26 16:19:31 -0500

    (scripts): Installation issues

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,30 +9,29 @@ if [ ! -d $dotfiles_dir ]; then
 fi
 
 # Install Homebrew
-command -v brew &> /dev/null || \
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install formulae/casks using Brewfile
 if [ $(/usr/bin/uname -m) = "arm64" ]; then # Apple Silicon macOS
-  HOMEBREW_PREFIX="/opt/homebrew"
+  HOMEBREW_PREFIX="/opt/homebrew/bin"
 else # Intel macOS
-  HOMEBREW_PREFIX="/usr/local"
+  HOMEBREW_PREFIX="/usr/local/bin"
 fi
 
-$HOMEBREW_PREFIX/bin/brew bundle install --file=$dotfiles_dir/Brewfile
-$HOMEBREW_PREFIX/bin/brew cleanup
+command -v $HOMEBREW_PREFIX/brew &> /dev/null || \
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+$HOMEBREW_PREFIX/brew bundle install --file=$dotfiles_dir/Brewfile
+$HOMEBREW_PREFIX/brew cleanup
 
   # ZSH won't load properly if $ZDOTDIR is being set in .zsh/.zprofile.
   # Therefore, ~/.zshenv needs to exist and have set the variable, before
   # everything else is sourced.
 if [ ! -f $HOME/.zshenv ]; then
-  echo 'export ZDOTDIR=$HOME/.zshenv' > $HOME/.zshenv
+  echo 'export ZDOTDIR=$HOME/.zsh' > $HOME/.zshenv
 else
-  grep -q 'export ZDOTDIR=$HOME/.zshenv' $HOME/.zshenv || \
-    echo 'export ZDOTDIR=$HOME/.zshenv' >> $HOME/.zshenv
+  grep -q 'export ZDOTDIR=$HOME/.zsh' $HOME/.zshenv || \
+    echo 'export ZDOTDIR=$HOME/.zsh' >> $HOME/.zshenv
 fi
 
-$HOMEBREW_PREFIX/bin/bash $dotfiles_dir/scripts/link_config.sh
+$HOMEBREW_PREFIX/bash $dotfiles_dir/scripts/link_config.sh
 
 # Vim plugins
 [ ! -d $HOME/.vim/bundle/Vundle.vim ] && \
