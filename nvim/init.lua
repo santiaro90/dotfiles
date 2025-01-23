ba765af709f14c531e3dee8e2af9b779e405c87f
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-01-23 06:20:51 -0500

    (vscode): Give up on VSCode + NeoVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,4 @@
-if vim.g.vscode then
-  require("config.vscode")
-else
-  require("config.editor")
-  require("config.keybindings")
-  require("plugins")
-  require("config.colours")
-end
+require("config.editor")
+require("config.keybindings")
+require("plugins")
+require("config.colours")
