822d923ac7f5a0653aa24f2d79dbb7956b22e907
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-02-02 09:52:02 -0500

    (neovim): Fix format command for Python

--- nvim/lua/plugins/code_quality.lua
+++ nvim/lua/plugins/code_quality.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
     opts = {
+      log_level = vim.log.levels.INFO,
       notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don"t
@@ -25,7 +26,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "ruff", "ruff_organize_imports" },
+        python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
         sh = { "shfmt" },
       },
     },
