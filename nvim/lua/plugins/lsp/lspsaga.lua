91b2ef1bff350c78f9651d1257ddba7627de6a2a
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-02-22 21:36:23 -0500

    (neovim): Remove annoying lightbulb sign

--- nvim/lua/plugins/lsp/lspsaga.lua
+++ nvim/lua/plugins/lsp/lspsaga.lua
@@ -6,31 +6,30 @@ return {
       { "nvim-tree/nvim-web-devicons" },
       { "nvim-treesitter/nvim-treesitter" },
     },
-    opts = function()
-      return {
-        definition = {
-          keys = { quit = "<C-[>" },
-        },
-        finder = {
-          keys = { quit = "<C-[>", toggle_or_open = "o" },
-        },
-        outline = {
-          keys = { jump = "<CR>", quit = "<C-[>", toggle_or_jump = "o" },
-          layout = "float",
-        },
-        rename = {
-          keys = { quit = "<C-[>" },
-        },
-        lightbulb = {
-          virtual_text = false,
-        },
-        symbol_in_winbar = {
-          enable = false,
-        },
-        ui = {
-          title = false,
-        },
-      }
-    end,
+    opts = {
+      definition = {
+        keys = { quit = "<C-[>" },
+      },
+      finder = {
+        keys = { quit = "<C-[>", toggle_or_open = "o" },
+      },
+      outline = {
+        keys = { jump = "<CR>", quit = "<C-[>", toggle_or_jump = "o" },
+        layout = "float",
+      },
+      rename = {
+        keys = { quit = "<C-[>" },
+      },
+      lightbulb = {
+        virtual_text = false,
+        sign = false,
+      },
+      symbol_in_winbar = {
+        enable = false,
+      },
+      ui = {
+        title = false,
+      },
+    },
   },
 }
