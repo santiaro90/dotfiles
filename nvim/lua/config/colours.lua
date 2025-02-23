e736a5d63308dfe043db3823a38effd2477c6dbb
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-02-22 21:35:46 -0500

    (neovim): Undercurl for diagnostic errors/warnings

--- nvim/lua/config/colours.lua
+++ nvim/lua/config/colours.lua
@@ -1,3 +1,4 @@
+-- Better colours for diffs
 vim.api.nvim_set_hl(0, "DiffAdd", {
   bg = "#024f02",
 })
@@ -12,4 +13,15 @@ vim.api.nvim_set_hl(0, "DiffDelete", {
 
 vim.api.nvim_set_hl(0, "DiffText", {
   bg = "#048204",
+})
+
+-- Curly underlines for errors/warnings
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", {
+  undercurl = true,
+  sp = "#e5c890",
+})
+
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
+  undercurl = true,
+  sp = "#e78284",
 })
