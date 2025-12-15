501c620cc3307a57de2d088a4797b271c2ec7e91
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-12-15 10:14:35 -0500

    Remove avante plugin

--- nvim/lua/plugins/ai/init.lua
+++ nvim/lua/plugins/ai/init.lua
@@ -1,6 +1,5 @@
 local utils = require("config.utils")
 
-local avante = require("plugins.ai.avante")
 local copilot = require("plugins.ai.copilot")
 
-return utils.merge_plugins({ avante, copilot })
+return utils.merge_plugins({ copilot })
