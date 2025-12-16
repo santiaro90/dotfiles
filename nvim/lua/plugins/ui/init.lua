3b08accd383a3abe23ee1fbd162a5754de1394f5
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2025-12-15 19:03:41 -0500

    (neovim): Add zen mode plugin

--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -3,11 +3,13 @@ local utils = require("config.utils")
 local bufferline = require("plugins.ui.bufferline")
 local colourscheme = require("plugins.ui.colourscheme")
 local lualine = require("plugins.ui.lualine")
+local twilight = require("plugins.ui.twilight")
 local which_key = require("plugins.ui.which_key")
 
 return utils.merge_plugins({
   bufferline,
   colourscheme,
   lualine,
-  which_key
+  twilight,
+  which_key,
 })
