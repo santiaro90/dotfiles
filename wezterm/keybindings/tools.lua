ab2135fbcb74cc10ef38e3bb53b6ea4e49256835
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2026-01-28 12:56:41 -0500

    (wezterm): Add keybinding for icon selector

--- wezterm/keybindings/tools.lua
+++ wezterm/keybindings/tools.lua
@@ -6,6 +6,9 @@ module.apply = function(config)
   local keys = {
     { key = "d", mods = "SUPER", action = action.ShowDebugOverlay },
     { key = "p", mods = "SUPER", action = action.ActivateCommandPalette },
+    { key = "i", mods = "SUPER", action = action.CharSelect },
+    -- Shift+Enter for multi-line prompts when using Claude Code
+    { key = "Enter", mods = "SHIFT", action = action({ SendString = "\x1b\r" }) },
   }
 
   utils.append_to_list(config.keys, keys)
