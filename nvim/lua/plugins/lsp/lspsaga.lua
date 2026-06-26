e743c00138a827103477cabb61b038c29274ebad
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2026-06-26 16:58:59 -0500

    (nvim): Filter symbol outline to declarations

--- nvim/lua/plugins/lsp/lspsaga.lua
+++ nvim/lua/plugins/lsp/lspsaga.lua
@@ -16,6 +16,15 @@ return {
       outline = {
         keys = { jump = "<CR>", quit = "<C-[>", toggle_or_jump = "o" },
         layout = "float",
+        -- Show only functions, classes, methods, constructors, interfaces
+        symbol_filter = {
+          ignore = {
+            "Variable", "Constant", "Property", "Field", "EnumMember",
+            "Module", "Namespace", "Package", "File", "String", "Number",
+            "Boolean", "Array", "Object", "Key", "Null", "Event",
+            "Operator", "TypeParameter", "Enum",
+          },
+        },
       },
       rename = {
         keys = { quit = "<C-[>" },
