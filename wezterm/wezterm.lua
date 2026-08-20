cbc255cd38a52846a2f7e81c0a3abeafaf11dfd9
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2026-08-20 06:12:37 -0500

    (wezterm): Open terminal in WEZTERM_START_DIR or home

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.default_cwd = os.getenv("WEZTERM_START_DIR") or wezterm.home_dir
+
 require("ui").apply(config)
 require("keybindings").apply(config)
 
